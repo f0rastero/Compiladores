d754732f0d122848eb57bc9c6a1e4884f6efb914
Miguel Enoc Ramos Bañuelos <miguelrb0225@gmail.com>
2021-12-15 00:06:59 -0600

    Se creo la clase
    Se creó la clase transición y se agregaron las preguntas teóricas

---
+++ Practica1/Transicion.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Transicion.h"
+using namespace std
+
+//constructor
+Transicion::Transicion(int,int,char){
+
+
+}
---
+++ Practica1/Transicion.h
@@ -0,0 +1,15 @@
+using namespace std;
+
+class Transicion{
+
+    private:
+        int inicio, fin;
+        char epsilon;
+    public:
+        Transicion(int,int,char);
+        int getInicio();
+        
+
+
+
+}
